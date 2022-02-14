abbf30712618c23c6ce288ab440b3a34feb463b4
Artem <artem.yasevich@mail.ru>
2022-02-14 12:48:36 +0300

    Lesson 1: sortTemperatures() and sortSequence()

--- src/lesson1/JavaTasks.java
+++ src/lesson1/JavaTasks.java
@@ -2,36 +2,41 @@ package lesson1;
 
 import kotlin.NotImplementedError;
 
+import java.io.*;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.*;
+
 @SuppressWarnings("unused")
 public class JavaTasks {
     /**
      * Сортировка времён
-     *
+     * <p>
      * Простая
      * (Модифицированная задача с сайта acmp.ru)
-     *
+     * <p>
      * Во входном файле с именем inputName содержатся моменты времени в формате ЧЧ:ММ:СС AM/PM,
      * каждый на отдельной строке. См. статью википедии "12-часовой формат времени".
-     *
+     * <p>
      * Пример:
-     *
+     * <p>
      * 01:15:19 PM
      * 07:26:57 AM
      * 10:00:03 AM
      * 07:56:14 PM
      * 01:15:19 PM
      * 12:40:31 AM
-     *
+     * <p>
      * Отсортировать моменты времени по возрастанию и вывести их в выходной файл с именем outputName,
      * сохраняя формат ЧЧ:ММ:СС AM/PM. Одинаковые моменты времени выводить друг за другом. Пример:
-     *
+     * <p>
      * 12:40:31 AM
      * 07:26:57 AM
      * 10:00:03 AM
      * 01:15:19 PM
      * 01:15:19 PM
      * 07:56:14 PM
-     *
+     * <p>
      * В случае обнаружения неверного формата файла бросить любое исключение.
      */
     static public void sortTimes(String inputName, String outputName) {
@@ -40,28 +45,28 @@ public class JavaTasks {
 
     /**
      * Сортировка адресов
-     *
+     * <p>
      * Средняя
-     *
+     * <p>
      * Во входном файле с именем inputName содержатся фамилии и имена жителей города с указанием улицы и номера дома,
      * где они прописаны. Пример:
-     *
+     * <p>
      * Петров Иван - Железнодорожная 3
      * Сидоров Петр - Садовая 5
      * Иванов Алексей - Железнодорожная 7
      * Сидорова Мария - Садовая 5
      * Иванов Михаил - Железнодорожная 7
-     *
+     * <p>
      * Людей в городе может быть до миллиона.
-     *
+     * <p>
      * Вывести записи в выходной файл outputName,
      * упорядоченными по названию улицы (по алфавиту) и номеру дома (по возрастанию).
      * Людей, живущих в одном доме, выводить через запятую по алфавиту (вначале по фамилии, потом по имени). Пример:
-     *
+     * <p>
      * Железнодорожная 3 - Петров Иван
      * Железнодорожная 7 - Иванов Алексей, Иванов Михаил
      * Садовая 5 - Сидоров Петр, Сидорова Мария
-     *
+     * <p>
      * В случае обнаружения неверного формата файла бросить любое исключение.
      */
     static public void sortAddresses(String inputName, String outputName) {
@@ -70,14 +75,14 @@ public class JavaTasks {
 
     /**
      * Сортировка температур
-     *
+     * <p>
      * Средняя
      * (Модифицированная задача с сайта acmp.ru)
-     *
+     * <p>
      * Во входном файле заданы температуры различных участков абстрактной планеты с точностью до десятых градуса.
      * Температуры могут изменяться в диапазоне от -273.0 до +500.0.
      * Например:
-     *
+     * <p>
      * 24.7
      * -12.6
      * 121.3
@@ -85,11 +90,11 @@ public class JavaTasks {
      * 99.5
      * -12.6
      * 11.0
-     *
+     * <p>
      * Количество строк в файле может достигать ста миллионов.
      * Вывести строки в выходной файл, отсортировав их по возрастанию температуры.
      * Повторяющиеся строки сохранить. Например:
-     *
+     * <p>
      * -98.4
      * -12.6
      * -12.6
@@ -98,18 +103,46 @@ public class JavaTasks {
      * 99.5
      * 121.3
      */
+    //Сложность O(n)
+    //Ресурсоёмкость O(1)
     static public void sortTemperatures(String inputName, String outputName) {
-        throw new NotImplementedError();
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(inputName)));
+             FileWriter writer = new FileWriter(outputName)) {
+
+            int[] repeatings = new int[7731];
+            String line;
+            while ((line = reader.readLine()) != null) {
+                int index = (int) ((roundToOnePlace(Double.parseDouble(line) + 273)) * 10);
+                repeatings[index]++;
+            }
+            reader.close();
+            for (int i = 0; i < repeatings.length; i++) {
+                for (int j = 0; j < repeatings[i]; j++) {
+                    double temperature = i / 10.0 - 273.0;
+                    writer.write(roundToOnePlace(temperature) + "\n");
+
+                }
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static double roundToOnePlace(double value) {
+        BigDecimal bd = BigDecimal.valueOf(value);
+        bd = bd.setScale(1, RoundingMode.HALF_UP);
+        return bd.doubleValue();
     }
 
     /**
      * Сортировка последовательности
-     *
+     * <p>
      * Средняя
      * (Задача взята с сайта acmp.ru)
-     *
+     * <p>
      * В файле задана последовательность из n целых положительных чисел, каждое в своей строке, например:
-     *
+     * <p>
      * 1
      * 2
      * 3
@@ -117,12 +150,12 @@ public class JavaTasks {
      * 3
      * 1
      * 2
-     *
+     * <p>
      * Необходимо найти число, которое встречается в этой последовательности наибольшее количество раз,
      * а если таких чисел несколько, то найти минимальное из них,
      * и после этого переместить все такие числа в конец заданной последовательности.
      * Порядок расположения остальных чисел должен остаться без изменения.
-     *
+     * <p>
      * 1
      * 3
      * 3
@@ -131,22 +164,67 @@ public class JavaTasks {
      * 2
      * 2
      */
+    //Сложность O(nlog(n))
+    //Ресурсоёмкость O(n)
     static public void sortSequence(String inputName, String outputName) {
-        throw new NotImplementedError();
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(inputName)));
+             FileWriter fileWriter = new FileWriter(outputName)) {
+
+            List<Integer> arr = new ArrayList<>();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                arr.add(Integer.parseInt(line));
+            }
+            reader.close();
+            if (arr.isEmpty()) {
+                return;
+            }
+
+            List<Integer> sorted = new ArrayList<>(arr);
+            sorted.sort(Integer::compareTo);
+            int maxRepeatings = 0;
+            int repeatedNumber = Integer.MAX_VALUE;
+            int currentRepeatings = 1;
+            int prevNumber = arr.get(0);
+
+            for (int i = 1; i < sorted.size(); i++) {
+                int number = sorted.get(i);
+                if ((number == prevNumber) && !(i == arr.size() - 1)) {
+                    currentRepeatings++;
+                    continue;
+                }
+                if (currentRepeatings > maxRepeatings) {
+                    repeatedNumber = prevNumber;
+                    maxRepeatings = currentRepeatings;
+                }
+                prevNumber = number;
+                currentRepeatings = 1;
+            }
+            for (int number : arr) {
+                if (number != repeatedNumber) {
+                    fileWriter.write(number + "\n");
+                }
+            }
+            for (int i = 0; i < maxRepeatings; i++) {
+                fileWriter.write(repeatedNumber + "\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     /**
      * Соединить два отсортированных массива в один
-     *
+     * <p>
      * Простая
-     *
+     * <p>
      * Задан отсортированный массив first и второй массив second,
      * первые first.size ячеек которого содержат null, а остальные ячейки также отсортированы.
      * Соединить оба массива в массиве second так, чтобы он оказался отсортирован. Пример:
-     *
+     * <p>
      * first = [4 9 15 20 28]
      * second = [null null null null null 1 3 9 13 18 23]
-     *
+     * <p>
      * Результат: second = [1 3 4 9 9 13 15 20 23 28]
      */
     static <T extends Comparable<T>> void mergeArrays(T[] first, T[] second) {
--- test/lesson1/AbstractTaskTests.kt
+++ test/lesson1/AbstractTaskTests.kt
@@ -136,6 +136,24 @@ abstract class AbstractTaskTests : AbstractFileTests() {
         } finally {
             File("temp.txt").delete()
         }
+        try {
+            sortTemperatures("input/temp_in2.txt", "temp.txt")
+            assertFileContent(
+                "temp.txt",
+                """
+                    -273.0
+                    -273.0
+                    -272.8
+                    -272.7
+                    272.0
+                    499.9
+                    499.9
+                    500.0
+                """.trimIndent()
+            )
+        } finally {
+            File("temp.txt").delete()
+        }
 
         fun testGeneratedTemperatures(size: Int): PerfResult<Unit> {
             try {
@@ -298,6 +316,26 @@ abstract class AbstractTaskTests : AbstractFileTests() {
         try {
             sortSequence("input/empty.txt", "temp.txt")
             assertFileContent("temp.txt", "")
+        } finally {
+            File("temp.txt").delete()
+        }
+        try {
+            sortSequence("input/seq_in6.txt", "temp.txt")
+            assertFileContent(
+                "temp.txt",
+                """
+                        1
+                        2
+                        6
+                        6
+                        6
+                        6
+                        5
+                        5
+                        5
+                        5
+                    """.trimIndent()
+            )
         } finally {
             File("temp.txt").delete()
         }
